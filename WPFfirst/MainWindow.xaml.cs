19269a15f37594719a7e92150a588ab1b0550bb8
TatsuyaKashiwa <sachikaze417@gmail.com>
2024-10-02 20:26:39 +0900

    ボタンに機能追加完了

--- WPFfirst/MainWindow.xaml.cs
+++ WPFfirst/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,54 @@ namespace WPFfirst
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.classification.Source = null;
+            this.destination.Source = null;
+        }
+
+        private void changeColor_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush Yellow = new SolidColorBrush(Colors.Yellow);
+            SolidColorBrush YellowGreen = new SolidColorBrush(Colors.YellowGreen);
+            if (this.Line.Background.ToString() == "#FF9ACD32")
+            {
+                this.Line.Background = Yellow;
+            }
+            else 
+            {
+                this.Line.Background = YellowGreen;
+            }
+        }
+
+        private void changeClassification_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog classification = new OpenFileDialog();
+            bool? DialogResult = classification.ShowDialog();
+            if (DialogResult == true) {
+                Uri uri = new Uri(classification.FileName);
+                BitmapImage bmp = new BitmapImage(uri);
+                this.classification.Source = bmp;
+            }
+        }
+
+        private void changeDirection_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog destination = new OpenFileDialog();
+            bool? DialogResult = destination.ShowDialog();
+            if (DialogResult == true)
+            {
+                Uri uri = new Uri(destination.FileName);
+                BitmapImage bmp = new BitmapImage(uri);
+                this.destination.Source = bmp;
+            }
         }
     }
 }
